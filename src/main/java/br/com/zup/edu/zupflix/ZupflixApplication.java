91c084b2ceaeec5b22fb4e221576f3b26a2168b4
Denes <jose.araujo@zup.com.br>
2022-04-12 08:47:49 -0300

    commit inicial

---
+++ src/main/java/br/com/zup/edu/zupflix/ZupflixApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.zupflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/zupflix/ZupflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.zupflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
